468eb5fef64eff5064f176983ea41af09ea1e492
Bruno Ferraz Silveira <bfssfb@outlook.com.br>
2019-12-05 11:54:17 -0300

    Add HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { HashRouter, Route } from 'react-router-dom'
 
 import Header from './Header'
 import Home from './Home'
@@ -7,11 +7,11 @@ import Detalhes from './Detalhes'
 
 function App() {
   return (
-    <Router>
+    <HashRouter basename='/'>
       <Header />
       <Route path='/' exact component={Home} />
       <Route path='/detalhes/:id' component={Detalhes} />
-    </Router>
+    </HashRouter>
   )
 }
 
