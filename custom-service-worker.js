f72cce0244710a15eea32e6bbd4d62f33c8300c5
Bruno Ferraz Silveira <bfssfb@outlook.com.br>
2019-12-05 19:33:54 -0300

    Updates

--- custom-service-worker.js
+++ custom-service-worker.js
@@ -11,7 +11,12 @@ workbox.routing.registerRoute(
     new workbox.strategies.NetworkFirst()
 )
 
+// workbox.routing.registerRoute(
+//     'http://localhost:3000/',
+//     new workbox.strategies.NetworkFirst()
+// )
+
 workbox.routing.registerRoute(
-    'http://localhost:3000/',
+    'https://brunofsilveira.github.io/Quadrinhos-Avenida/',
     new workbox.strategies.NetworkFirst()
 )
