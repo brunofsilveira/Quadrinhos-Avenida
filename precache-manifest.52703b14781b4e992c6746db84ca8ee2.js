677e66a93466e9f2bcf656a515c86896ec27f2be
Bruno Ferraz Silveira <bfssfb@outlook.com.br>
2019-12-05 15:19:37 -0300

    Updates

---
+++ precache-manifest.52703b14781b4e992c6746db84ca8ee2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bced4535abe97eee02122a5afff41fa5",
+    "url": "/quadrinhos-avenida/index.html"
+  },
+  {
+    "revision": "4cbbefa7df5fb9731757",
+    "url": "/quadrinhos-avenida/static/js/2.2d1d44e9.chunk.js"
+  },
+  {
+    "revision": "2a385d7a0e6f692bc250",
+    "url": "/quadrinhos-avenida/static/js/main.65cae9e6.chunk.js"
+  },
+  {
+    "revision": "bcc63b5495f8be683c8e",
+    "url": "/quadrinhos-avenida/static/js/runtime-main.f0128bf6.js"
+  }
+]);
